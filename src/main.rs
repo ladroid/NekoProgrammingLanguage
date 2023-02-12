71210766d35b9528366a29f0d54a6e836aefc8e1
Maxence Lequeux <maxenceleq@gmail.com>
2023-02-12 07:57:25 +0100

    Check for .nek extension

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@ use my_project::interpreter::Interpreter;
 
 static ERROR_MISSING_INPUT_FILE: &str = "Missing input file";
 static ERROR_FILE_READ: &str = "Could not read file";
+static ERROR_BAD_EXTENSION: &str = "Source files must have the .nek file extension";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     let source_file = args.iter().skip(1).next().expect(ERROR_MISSING_INPUT_FILE);
+    
+    if ! source_file.ends_with(".nek"){
+        panic!("{}", ERROR_BAD_EXTENSION);
+    }
+
     let code = fs::read_to_string(source_file).expect(&format!("{} {}", ERROR_FILE_READ, source_file));
 
     let mut interpreter = Interpreter::new();
